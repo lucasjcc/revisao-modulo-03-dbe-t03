6b44655b835f28dec0f7a4dfe9b0f425b804744b
Lucas Carvalho <lucasjorge.cap@gmail.com>
2023-10-16 21:49:30 -0300

    dump.sql atualizado

--- dump.sql
+++ dump.sql
@@ -1,14 +1,25 @@
 CREATE DATABASE roteiro;
+DROP DATABASE roteiro;
 
 CREATE TABLE usuarios (
 	id serial primary key,
-    nome varchar(100) not null,
-    email varchar(70) not null unique,
-    senha text not null
+  nome varchar(100) not null,
+  email varchar(70) not null unique,
+  senha text not null
 );
+DROP TABLE usuarios;
 
 
 INSERT INTO roteiro
     (nome, email, senha)
 VALUES
     ('gobis', 'gobis@gmail.com', '123');
+
+SELECT 
+	* 
+FROM 
+	usuarios
+WHERE
+	email = 'caldeira@gmail.com' AND id <> 2;
+  
+SELECT * FROM usuarios WHERE id = 1;
